e164c7b9d04c64be1c2c6db096109fe8f3f18190
David Ung <david.ung@learnet.se>
2024-11-21 10:47:21 +0100

    test

--- ShooterFeedback/Assets/Scripts/Enemy1.cs
+++ ShooterFeedback/Assets/Scripts/Enemy1.cs
@@ -2,15 +2,57 @@ using UnityEngine;
 
 public class Enemy1 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Movement")]
+    [SerializeField] float moveSpeed;
+
+
+    [Header("Gizmos")]
+    [SerializeField] Vector2 wallCheckPos;
+    [SerializeField] Vector2 groundCheckPos;
+    [SerializeField] LayerMask checkMask;
+
+    [SerializeField] Color checkPosColors = Color.red;
+    [SerializeField] float checkPosSize = 0.1f;
+
+    private bool isFacingRight = true;
+    
+
+    private void FixedUpdate()
     {
-        
+        if (CollisionCheck(wallCheckPos) || !CollisionCheck(groundCheckPos))
+        {
+            isFacingRight = !isFacingRight;
+        }
+
+        float horizontalVelocity = (isFacingRight) ? moveSpeed : -moveSpeed;
+      //  rb.linearVelocity = new Vector2(horizontalVelocity, rb.linearVelocity.y);
     }
 
-    // Update is called once per frame
-    void Update()
+    private bool CollisionCheck(Vector2 positionToCheck)
     {
-        
+        positionToCheck = (Vector2)transform.position + new Vector2((isFacingRight) ? positionToCheck.x : -positionToCheck.x, positionToCheck.y);
+
+        if (Physics2D.OverlapCircle(positionToCheck, 0.1f, checkMask))
+        {
+            return true;
+        }
+
+        return false;
+    }
+    private void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = checkPosColors;
+
+        Vector2 myPosition = (Vector2)transform.position;
+        Vector2 wallCheckActualPos = myPosition + new Vector2((isFacingRight) ? wallCheckPos.x : -wallCheckPos.x, wallCheckPos.y);
+        Vector2 groundCheckActualPos = myPosition + new Vector2((isFacingRight) ? groundCheckPos.x : -groundCheckPos.x, groundCheckPos.y);
+        Gizmos.DrawWireSphere(wallCheckActualPos, checkPosSize);
+        Gizmos.DrawWireSphere(groundCheckActualPos, checkPosSize);
     }
 }
+
