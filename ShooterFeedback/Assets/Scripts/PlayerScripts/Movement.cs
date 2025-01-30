f72198901be1efa2b49450e8f0f2f40822f02403
Adonai <adonai.osterberg@elev.ga.lbs.se>
2025-01-30 10:48:24 +0100

    showcase level

--- ShooterFeedback/Assets/Scripts/PlayerScripts/Movement.cs
+++ ShooterFeedback/Assets/Scripts/PlayerScripts/Movement.cs
@@ -37,8 +37,6 @@ public class Movement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        
-        walkSource = GetComponentInChildren<AudioSource>();
     }
 
 
